7d0764a9f1a20ec6f1f13a085b2331cf8e2ef6d9
Alexander Holtkamp <alexander.holtkamp@coremedia.com>
2021-11-11 20:03:06 +0100

    Migrated studio-client part to NPM/TypeScript

---
+++ studio-client/apps/main/content-hub-adapter-youtube/src/ContentHubStudioYoutubePlugin.ts
@@ -0,0 +1,30 @@
+import Config from "@jangaroo/runtime/Config";
+import ContentHubYoutube_properties from "./ContentHubYoutube_properties";
+import ContentHub_properties from "@coremedia/studio-client.main.content-hub-editor-components/ContentHub_properties";
+import CopyResourceBundleProperties from "@coremedia/studio-client.main.editor-components/configuration/CopyResourceBundleProperties";
+import StudioPlugin from "@coremedia/studio-client.main.editor-components/configuration/StudioPlugin";
+import ConfigUtils from "@jangaroo/runtime/ConfigUtils";
+import resourceManager from "@jangaroo/runtime/l10n/resourceManager";
+interface ContentHubStudioYoutubePluginConfig extends Config<StudioPlugin> {
+}
+
+
+
+    class ContentHubStudioYoutubePlugin extends StudioPlugin{
+  declare Config: ContentHubStudioYoutubePluginConfig;
+
+  static readonly xtype:string = "com.coremedia.labs.plugins.adapters.youtube.ContentHubStudioYoutubePlugin";
+
+  constructor(config:Config<ContentHubStudioYoutubePlugin> = null){
+    super( ConfigUtils.apply(Config(ContentHubStudioYoutubePlugin, {
+
+  configuration:[
+    new CopyResourceBundleProperties({
+            destination: resourceManager.getResourceBundle(null,ContentHub_properties),
+            source: resourceManager.getResourceBundle(null,ContentHubYoutube_properties)})
+  ]
+
+
+}),config));
+  }}
+export default ContentHubStudioYoutubePlugin;
